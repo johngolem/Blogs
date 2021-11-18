8a119969a08357b8709a4ef487f5719b7f279940
John Paul <johnpaul@trees.local>
2021-11-18 14:18:36 +0300

    add blueprint

---
+++ config.py
@@ -0,0 +1,56 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+    DEBUG = True
+    #Route to photos
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #email configurations
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  
+
+    DEBUG = True
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
+  DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
