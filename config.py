85b898292afd91b57d641cd95fad662a02bdb79d
John Paul <johnpaul@trees.local>
2021-11-22 10:18:21 +0300

    add email configuration

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("john.odero@student.moringaschool.com")
-    MAIL_PASSWORD = os.environ.get("dirtdiver69@")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 
 class ProdConfig(Config):
