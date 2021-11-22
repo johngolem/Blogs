e8d19d434cac701b6f3839a924525b0de5faf950
John Paul <johnpaul@trees.local>
2021-11-22 10:15:40 +0300

    add email configurations

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://johnpaul:trees@localhost/blogs'
     SECRET_KEY = '<blogsecretkey>'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("john.odero@student.moringaschool.com")
+    MAIL_PASSWORD = os.environ.get("dirtdiver69@")
     
 
 class ProdConfig(Config):
