6b25a6697b4c8056e1ede3169ccb70f43023850a
John Paul <johnpaul@trees.local>
2021-11-22 10:26:18 +0300

    remove start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://johnpaul:trees@localhost/blog'
-export MAIL_USERNAME=<john.odero@student.moringaschool.com>
-export MAIL_PASSWORD=<dirtdiver69@>
-python3 manage.py server
